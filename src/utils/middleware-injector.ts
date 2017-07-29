8beb4b1fab0523d366a997d67d7b15796cfa76d7
Humberto Barrientos González <hawksmoor@gmail.com>
2017-07-29 21:02:09 +0200

    Update middleware-injector.ts
    Only take in count "js" files and not other ones.

--- src/utils/middleware-injector.ts
+++ src/utils/middleware-injector.ts
@@ -3,8 +3,9 @@ import {ConcatSource} from "webpack-sources";
 export  default function middlewareInjector(compilation, source: string) {
     compilation.plugin('after-optimize-chunk-assets', chunks => chunks.forEach(chunk => {
         chunk.files.forEach(fileName => {
-            if(fileName === 'extract-text-webpack-plugin-output-filename') { return; }
-            compilation.assets[fileName] = new ConcatSource(source, compilation.assets[fileName]);
+            if(/\.js$/.test(fileName)) {
+              compilation.assets[fileName] = new ConcatSource(source, compilation.assets[fileName]);
+            }
         });
     }));
 }
