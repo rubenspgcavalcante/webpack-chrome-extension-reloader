37d8f4ee7b5a8663732e25bada735cc6fc638abb
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2017-11-23 12:09:49 +0100

    Some additions:  - Ignoring plugin on NODE_ENV == "production"  - Adding prettier to the project  - Adding Husky for git hooks  - Added Webpack Dashboard

--- src/ChromeExtensionReloader.ts
+++ src/ChromeExtensionReloader.ts
@@ -26,12 +26,14 @@ export default class ChromeExtensionReloader extends AbstractChromePluginReloade
     }
 
     apply(compiler) {
-        compiler.plugin("compilation", (comp) =>
-            comp.plugin('after-optimize-chunk-assets',
-                (chunks) => comp.assets = {...comp.assets, ...this._injector(comp.assets, chunks)}
-            ));
+        if (process.env.NODE_ENV !== "production") {
+            compiler.plugin("compilation", (comp) =>
+                comp.plugin('after-optimize-chunk-assets',
+                    (chunks) => comp.assets = {...comp.assets, ...this._injector(comp.assets, chunks)}
+                ));
 
-        compiler.plugin("after-emit", (comp, done) =>
-            this._triggerer(comp.hash).then(done).catch(done));
+            compiler.plugin("after-emit", (comp, done) =>
+                this._triggerer(comp.hash).then(done).catch(done));
+        }
     }
 }
