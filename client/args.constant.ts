5b699caee530a998c61e58b89bc19c8cb6a9cda6
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2018-01-18 14:50:19 +0100

    Formatting

--- client/args.constant.ts
+++ client/args.constant.ts
@@ -1,5 +1,5 @@
-export const CONFIG = 'config';
-export const PORT = 'port';
-export const NO_PAGE_RELOAD = 'no-page-reload';
-export const BACKGROUND_ENTRY = 'background';
-export const CONTENT_SCRIPT_ENTRY = 'content-script';
+export const CONFIG = "config";
+export const PORT = "port";
+export const NO_PAGE_RELOAD = "no-page-reload";
+export const BACKGROUND_ENTRY = "background";
+export const CONTENT_SCRIPT_ENTRY = "content-script";
--- client/index.ts
+++ client/index.ts
@@ -1,10 +1,16 @@
-import {install} from "source-map-support";
-import {resolve} from "path";
-import {cwd} from "process";
-import * as webpack from 'webpack';
+import { install } from "source-map-support";
+import { resolve } from "path";
+import { cwd } from "process";
+import * as webpack from "webpack";
 import * as minimist from "minimist";
 import ChromeExtensionReloader from "../src/ChromeExtensionReloader";
-import {BACKGROUND_ENTRY, CONFIG, CONTENT_SCRIPT_ENTRY, NO_PAGE_RELOAD, PORT} from "./args.constant";
+import {
+  BACKGROUND_ENTRY,
+  CONFIG,
+  CONTENT_SCRIPT_ENTRY,
+  NO_PAGE_RELOAD,
+  PORT
+} from "./args.constant";
 import {
   DEFAULT_BACKGROUND_ENTRY,
   DEFAULT_CONFIG,
@@ -14,42 +20,43 @@ import {
 
 install();
 
-const {_, ...params} = minimist(process.argv.slice(2));
+const { _, ...params } = minimist(process.argv.slice(2));
 
 const config = params[CONFIG] || DEFAULT_CONFIG;
 const port = params[PORT] || DEFAULT_PORT;
-const contentScript = params[CONTENT_SCRIPT_ENTRY] || DEFAULT_CONTENT_SCRIPT_ENTRY;
+const contentScript =
+  params[CONTENT_SCRIPT_ENTRY] || DEFAULT_CONTENT_SCRIPT_ENTRY;
 const background = params[BACKGROUND_ENTRY] || DEFAULT_BACKGROUND_ENTRY;
 
 const pluginOptions: PluginOptions = {
   port,
   reloadPage: !params[NO_PAGE_RELOAD],
-  entries: {contentScript, background}
+  entries: { contentScript, background }
 };
 
 const optPath = resolve(cwd(), config);
 let opts = {};
 
 try {
-// tslint:disable-next-line:no-eval
-  opts = eval('require')(optPath);
-}
-catch (err) {
+  // tslint:disable-next-line:no-eval
+  opts = eval("require")(optPath);
+} catch (err) {
   console.error(`[Error] Couldn't require the file: ${optPath}`);
   console.error(err);
-  process.exit()
+  process.exit();
 }
 
-const compiler = webpack(typeof opts === 'function' ? opts(process.env, process.argv) : opts);
+const compiler = webpack(
+  typeof opts === "function" ? opts(process.env, process.argv) : opts
+);
 new ChromeExtensionReloader(pluginOptions).apply(compiler);
 
 compiler.watch({}, (err, stats) => {
   if (err) {
     return treatErrors(err);
   }
-  console.info(stats.toString({colors: true}));
+  console.info(stats.toString({ colors: true }));
 });
-
 
 function treatErrors(err) {
   console.error(err.stack || err);
