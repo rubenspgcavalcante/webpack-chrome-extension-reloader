6958a4fbeaa2da5a6d1669ef56d91b3cb8de6a4d
Guy Serfaty <guy@poob.me>
2018-04-10 20:31:00 +0300

    add content script as array unit test

--- specs/middleware-injector.specs.ts
+++ specs/middleware-injector.specs.ts
@@ -12,12 +12,18 @@ describe("middleware-injector", () => {
 
   const entriesInfo = {
     background: {name: "bgChunkName", path: "./path/to/bg-script.js"},
-    contentScript: {name: "contentChunkName", path: "./path/to/content-script.js"}
+    contentScript: {name: "contentChunkName", path: "./path/to/content-script.js"},
+    contentScript2: {name: "contentChunkName2", path: "./path/to/content-script2.js"}
   };
 
   const options: EntriesOption = {
     background: entriesInfo.background.name,
     contentScript: entriesInfo.contentScript.name
+  };
+
+  const optionsMultipleCS: EntriesOption = {
+    background: entriesInfo.background.name,
+    contentScript: [entriesInfo.contentScript.name, entriesInfo.contentScript2.name]
   };
 
   const fakeCssPath = "./path/to/some.css";
@@ -26,6 +32,7 @@ describe("middleware-injector", () => {
   const assets = {
     [entriesInfo.background.path]: {source: () => "const bg = true;"},
     [entriesInfo.contentScript.path]: {source: () => "const cs = true;"},
+    [entriesInfo.contentScript2.path]: {source: () => "const cs = true;"},
     [fakeCssPath]: {source: () => "some-css-source"},
     [fakeImgPath]: {source: () => "some-base64-source"}
   };
@@ -49,6 +56,20 @@ describe("middleware-injector", () => {
 
     const newContentSource = newAssets[entriesInfo.contentScript.path].source();
     const oldContentSource = assets[entriesInfo.contentScript.path].source();
+    assert.equal(newContentSource, (sourceCode + oldContentSource));
+  });
+
+  it("Should find the content script entries and inject the middleware source on them in case CS as array", () => {
+    const assetsBuilderMultipleCS = middlewareInjector(optionsMultipleCS, sourceCode, sourceFactory);
+    const chunksMultipleCS = [
+      {name: optionsMultipleCS.background, files: [entriesInfo.background.path]},
+      {name: optionsMultipleCS.contentScript[1], files: [entriesInfo.contentScript2.path, fakeCssPath]},
+      {name: "someOtherAsset", files: [fakeImgPath]}
+    ];
+    const newAssets = assetsBuilderMultipleCS(assets, chunksMultipleCS);
+
+    const newContentSource = newAssets[entriesInfo.contentScript2.path].source();
+    const oldContentSource = assets[entriesInfo.contentScript2.path].source();
     assert.equal(newContentSource, (sourceCode + oldContentSource));
   });
 
