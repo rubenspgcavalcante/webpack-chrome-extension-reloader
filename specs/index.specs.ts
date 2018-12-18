120004b7ca6b17b09a022069359276de51274ee3
Rubens Pinheiro Gonçalves Cavalcante <rubenspgcavalcante@gmail.com>
2018-12-18 11:08:25 +0100

    Setup up tests for "fast reload" on new Chrome versions

--- specs/index.specs.ts
+++
@@ -1,7 +0,0 @@
-import {install} from "source-map-support";
-import "./middlewareSourceBuilder.specs";
-import "./middleware-injector.specs";
-import "./changesTriggerer.specs";
-import "./block-protection.specs";
-
-install();
